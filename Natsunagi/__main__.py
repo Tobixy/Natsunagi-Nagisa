3b6c4dfb04004d151ab26eef31ce753f278f465b
Tobixy <shivamrajput69bs@gmail.com>
2023-07-21 09:59:54 +0530

    Update __main__.py

--- Natsunagi/__main__.py
+++ Natsunagi/__main__.py
@@ -106,7 +106,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("Fumika.modules." + module_name)
+    imported_module = importlib.import_module("Natsunagi.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
